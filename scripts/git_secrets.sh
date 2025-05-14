1aaa5f20e918360dbd5500b0a403111d328ca14e
Sudeshna Dash <suddash@amazon.de>
2025-05-14 15:38:05 +0200

    fix: add test to allowed list

--- scripts/git_secrets.sh
+++ scripts/git_secrets.sh
@@ -4,4 +4,5 @@ set -e
 echo "Running git-secrets scan"
 git secrets --register-aws
 git secrets --add --allowed 'packages/constructs/L3/ai/gaia-l3-construct/lib/sagemaker-model/image-repository-mapping.ts:.*'
+git secrets --add --allowed 'installer/test/mdaa-installer.test.ts:.*'
 git secrets --scan --no-index
